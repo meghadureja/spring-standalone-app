7ac9829272b7b503d42e904dfe958335e0a60fa5
meghadureja <megha.dureja01@gmail.com>
2017-02-25 15:35:52 +0530

    adding spring dependency

---
+++ src/main/java/org/education/app/App.java
@@ -0,0 +1,17 @@
+package org.education.app;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        System.out.println(applicationContext);
+    }
+}
